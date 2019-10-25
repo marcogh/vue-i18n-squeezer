296e1569140202a776ccf2b4445b3773f909e038
Marco Ghidinelli <marcogh@gmail.com>
2019-10-25 17:39:55 +0200

    importLocales stub

--- bin/vue-i18n-exfiltrator.js
+++ bin/vue-i18n-exfiltrator.js
@@ -6,38 +6,56 @@ const path = require('path')
 const compiler = require('vue-template-compiler')
 const matchAll = require('string.prototype.matchall');
 
-matchAll.shim()
+matchAll.shim() // monkey patching missing matchAll
 
-parseTemplates: () => {
-
-}
+const basePath = path.resolve(process.cwd())
+const packageJson = JSON.parse(fs.readFileSync(path.join(basePath, 'package.json')).toString())
+const supportedLocales = packageJson.config['supported-locales']
+const defaultLocale = packageJson.config['default-locale']
 
 getTranslations = (content) => {
   //return content.match(/\$tc?[\r\n ]*["']\(.*\)["']/gm) || [];
   return content.matchAll(/\$tc? *\([\r\n ]*["']([^'"]+)["'][^\)]*\)/gm) || []
 }
 
-runImport = () => {
-  const basePath = path.resolve(process.cwd())
-  const packageJson = JSON.parse(fs.readFileSync(path.join(basePath, 'package.json')).toString())
-  //console.log(packageJson)
-  glob('src/**/*.vue', (err, files) => {
-    const translations = []
-    const pluralTranslations = []
-    files.forEach(file => {
-      //console.log(`checking ${file}`)
-      let content = fs.readFileSync(file).toString()
-      for(let match of getTranslations(content)){
-        if ( match[0].match(/\$tc.*/) ){
-          pluralTranslations.push(match[1])
-        } else {
-          translations.push(match[1])
-        }
-      }
-    })
-    console.log(JSON.stringify(translations))
-    console.log(JSON.stringify(pluralTranslations))
+importLocales = () => {
+  locales = []
+  supportedLocales.forEach( (item) => {
+    console.log(item)
+    localeFile = fs.readFileSync(path.join(basePath, `src/locales/${item}.json`))
+    locales.push(
+      JSON.parse(localeFile)
+    )
+    // load translation item
+    
   })
+  console.log(locales)
+}
+
+exportLocales = () => {
+
+}
+
+runImport = () => {
+  const translations = []
+  const pluralTranslations = []
+
+  let files = glob.sync('src/**/*.vue')
+  files.forEach(file => {
+    //console.log(`checking ${file}`)
+    let content = fs.readFileSync(file).toString()
+    for(let match of getTranslations(content)){
+      if ( match[0].match(/\$tc.*/) ){
+        pluralTranslations.push(match[1])
+      } else {
+        translations.push(match[1])
+      }
+    }
+  })
+  return JSON.stringify({
+    'translations': translations,
+    'plural_translations': pluralTranslations
+  }, null, 2)
 }
 
 runExport: () => {
@@ -46,11 +64,15 @@ runExport: () => {
 
 switch (process.argv[2]){
   case 'import':
-    runImport()
+    let imported = runImport()
+    console.log(imported)
+    importLocales()
     break
 
   case 'export':
-    runExport()
+    let exported = runExport()
+    console.log(exported)
+
     break
 
   default:
